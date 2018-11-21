dc70b6ae335d21875c0f3a7c8ba5362f989603d2
Prem Acharya <prem.acharya@hotmail.com>
2018-11-22 00:53:19 +1100

    test react lifecycle method added

---
+++ src/components/test/Test.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  componentWillMount() {
+    console.log("component Will mount ...");
+  }
+
+  componentDidMount() {
+    console.log("component did mount ...");
+  }
+
+  componentDidUpdate() {
+    console.log("component did update ...");
+  }
+
+  componentWillUpdate() {
+    console.log("component will update ...");
+  }
+
+  // UNSEF_.... //use prefix as componentWill... is deprecated
+  componentWillReceiveProps(nextProps, nextState) {
+    console.log("this run when component receives props");
+  }
+
+  // new lifecycle methods replacing componentWillReceiveProps
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return null;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Test;
